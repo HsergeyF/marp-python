274ab1646bb557ed4cb4cc9b1b0f52e133cb5757
HsergeyF <hsergey_frolov@mail.ru>
2023-02-06 19:13:35 +0300

    fix pres_builder

--- presentation_builder.py
+++ presentation_builder.py
@@ -3,18 +3,23 @@ class Presentation:
     _state = "--- \nmarp: true"
 
     def __init__(self, params: dict = {}) -> None:
+        """
+        If you prefer to set some params for all slides,
+        pass it during initialization and it will apply for the whole
+        presentation.
+        """
         self.is_initial_slide = True
         if params:
-            self.set_slide_params(params)
+            self._set_slide_params(params)
         self._end_slide()
 
-    def set_slide_params(self, params: dict) -> None:
+    def _set_slide_params(self, params: dict) -> None:
         for key, value in params.items():
             self._state += f"<!-- {key}: {value}--> \n"
 
     
     def add_slide(self, title: str, text:str, params: dict) -> None:
-        self.set_slide_params(params)
+        self._set_slide_params(params)
         self._state += f"# {title} \n"
         self._state += f" {text} \n"
         if self.is_initial_slide:
